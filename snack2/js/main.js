ff4c4b65fccd092575e70f5aec2f573905cc375a
Michele <micheleierniero@gmail.com>
2021-09-01 16:13:15 +0200

    snack 2 seconda soluzione

--- snack2/js/main.js
+++ snack2/js/main.js
@@ -3,7 +3,7 @@
 // Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 
 
-let teams = [
+const teams = [
     {
         nome: "Genoa",
         puntiFatti: 0,
@@ -32,38 +32,65 @@ let teams = [
 ];
 
 
+// PRIMA SOLUZIONE
+
 // Generare numeri random al posto degli 0 nelle proprietà:
 
 // punti fatti:
-let randomPuntiFatti = Math.floor(Math.random() * 100) + 1;
+// let randomPuntiFatti = Math.floor(Math.random() * 100) + 1;
 
 
-for (let i = 0; i < teams.length; i++) {
-    teams[i].puntiFatti = randomPuntiFatti;
-};
+// for (let i = 0; i < teams.length; i++) {
+//     teams[i].puntiFatti = randomPuntiFatti;
+// };
 
 
 // falli subiti:    
-let randomFalliSubiti = Math.floor(Math.random() * 100) + 1;
+// let randomFalliSubiti = Math.floor(Math.random() * 100) + 1;
 
-for (let i = 0; i < teams.length; i++) {
-    teams[i].falliSubiti = randomFalliSubiti;
+// for (let i = 0; i < teams.length; i++) {
+//     teams[i].falliSubiti = randomFalliSubiti;
+// };
+
+// console.log(teams);
+
+
+// usando la destrutturazione creiamo un nuovo array 
+// let [teamOne, teamTwo, teamThree, teamFour, teamFive] = teams;
+
+// i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+// for (let i = 0; i < teams.length; i++) {
+//     console.log("Nome squadra: " + teams[i].nome + " / " + "Falli subiti: " + teams[i].falliSubiti)
+// };
+
+
+
+// SECONDA SOLUZIONE
+
+// Generare numeri random al posto degli 0 nelle proprietà:
+
+const randomNumber = () => {
+    return Math.floor(Math.random() * 51);
 };
+
+// punti fatti:
+// falli subiti:  
+for(i = 0; i < teams.length; i++) {
+    teams[i].puntiFatti = randomNumber();
+    teams[i].falliSubiti = randomNumber();
+}
 
 console.log(teams);
 
 
 // usando la destrutturazione creiamo un nuovo array 
-let [teamOne, teamTwo, teamThree, teamFour, teamFive] = teams;
-
 // i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
-for (let i = 0; i < teams.length; i++) {
-    console.log("Nome squadra: " + teams[i].nome + " / " + "Falli subiti: " + teams[i].falliSubiti)
+
+const newTeams = [];
+
+for (i = 0; i < teams.length; i++) {
+    const {nome, falliSubiti} = teams[i];
+    newTeams.push({nome, falliSubiti});
 };
 
-
-
-
-
-
-
+console.log(newTeams);
